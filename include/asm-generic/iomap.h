a5180bad57bb0e21973019c9132488ffa78b6be3
Krzysztof Kozlowski <krzk@kernel.org>
2020-01-08 21:14:04 +0100

    WIP volatile
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- include/asm-generic/iomap.h
+++ include/asm-generic/iomap.h
@@ -26,14 +26,14 @@
  * in the low address range. Architectures for which this is not
  * true can't use this generic implementation.
  */
-extern unsigned int ioread8(const void __iomem *);
-extern unsigned int ioread16(const void __iomem *);
-extern unsigned int ioread16be(const void __iomem *);
-extern unsigned int ioread32(const void __iomem *);
-extern unsigned int ioread32be(const void __iomem *);
+extern unsigned int ioread8(const volatile void __iomem *);
+extern unsigned int ioread16(const volatile void __iomem *);
+extern unsigned int ioread16be(const volatile void __iomem *);
+extern unsigned int ioread32(const volatile void __iomem *);
+extern unsigned int ioread32be(const volatile void __iomem *);
 #ifdef CONFIG_64BIT
-extern u64 ioread64(const void __iomem *);
-extern u64 ioread64be(const void __iomem *);
+extern u64 ioread64(const volatile void __iomem *);
+extern u64 ioread64be(const volatile void __iomem *);
 #endif
 
 #ifdef readq
@@ -41,20 +41,20 @@ extern u64 ioread64be(const void __iomem *);
 #define ioread64_hi_lo ioread64_hi_lo
 #define ioread64be_lo_hi ioread64be_lo_hi
 #define ioread64be_hi_lo ioread64be_hi_lo
-extern u64 ioread64_lo_hi(const void __iomem *addr);
-extern u64 ioread64_hi_lo(const void __iomem *addr);
-extern u64 ioread64be_lo_hi(const void __iomem *addr);
-extern u64 ioread64be_hi_lo(const void __iomem *addr);
+extern u64 ioread64_lo_hi(const volatile void __iomem *addr);
+extern u64 ioread64_hi_lo(const volatile void __iomem *addr);
+extern u64 ioread64be_lo_hi(const volatile void __iomem *addr);
+extern u64 ioread64be_hi_lo(const volatile void __iomem *addr);
 #endif
 
-extern void iowrite8(u8, void __iomem *);
-extern void iowrite16(u16, void __iomem *);
-extern void iowrite16be(u16, void __iomem *);
-extern void iowrite32(u32, void __iomem *);
-extern void iowrite32be(u32, void __iomem *);
+extern void iowrite8(u8, volatile void __iomem *);
+extern void iowrite16(u16, volatile void __iomem *);
+extern void iowrite16be(u16, volatile void __iomem *);
+extern void iowrite32(u32, volatile void __iomem *);
+extern void iowrite32be(u32, volatile void __iomem *);
 #ifdef CONFIG_64BIT
-extern void iowrite64(u64, void __iomem *);
-extern void iowrite64be(u64, void __iomem *);
+extern void iowrite64(u64, volatile void __iomem *);
+extern void iowrite64be(u64, volatile void __iomem *);
 #endif
 
 #ifdef writeq
@@ -62,10 +62,10 @@ extern void iowrite64be(u64, void __iomem *);
 #define iowrite64_hi_lo iowrite64_hi_lo
 #define iowrite64be_lo_hi iowrite64be_lo_hi
 #define iowrite64be_hi_lo iowrite64be_hi_lo
-extern void iowrite64_lo_hi(u64 val, void __iomem *addr);
-extern void iowrite64_hi_lo(u64 val, void __iomem *addr);
-extern void iowrite64be_lo_hi(u64 val, void __iomem *addr);
-extern void iowrite64be_hi_lo(u64 val, void __iomem *addr);
+extern void iowrite64_lo_hi(u64 val, volatile void __iomem *addr);
+extern void iowrite64_hi_lo(u64 val, volatile void __iomem *addr);
+extern void iowrite64be_lo_hi(u64 val, volatile void __iomem *addr);
+extern void iowrite64be_hi_lo(u64 val, volatile void __iomem *addr);
 #endif
 
 /*
@@ -79,13 +79,19 @@ extern void iowrite64be_hi_lo(u64 val, void __iomem *addr);
  * memory across multiple ports, use "memcpy_toio()"
  * and friends.
  */
-extern void ioread8_rep(const void __iomem *port, void *buf, unsigned long count);
-extern void ioread16_rep(const void __iomem *port, void *buf, unsigned long count);
-extern void ioread32_rep(const void __iomem *port, void *buf, unsigned long count);
+extern void ioread8_rep(const volatile void __iomem *port, void *buf,
+			unsigned long count);
+extern void ioread16_rep(const volatile void __iomem *port, void *buf,
+			 unsigned long count);
+extern void ioread32_rep(const volatile void __iomem *port, void *buf,
+			 unsigned long count);
 
-extern void iowrite8_rep(void __iomem *port, const void *buf, unsigned long count);
-extern void iowrite16_rep(void __iomem *port, const void *buf, unsigned long count);
-extern void iowrite32_rep(void __iomem *port, const void *buf, unsigned long count);
+extern void iowrite8_rep(volatile void __iomem *port, const void *buf,
+			 unsigned long count);
+extern void iowrite16_rep(volatile void __iomem *port, const void *buf,
+			  unsigned long count);
+extern void iowrite32_rep(volatile void __iomem *port, const void *buf,
+			  unsigned long count);
 
 #ifdef CONFIG_HAS_IOPORT_MAP
 /* Create a virtual mapping cookie for an IO port range */
--- include/linux/io-64-nonatomic-lo-hi.h
+++ include/linux/io-64-nonatomic-lo-hi.h
@@ -57,7 +57,7 @@ static inline void lo_hi_writeq_relaxed(__u64 val, volatile void __iomem *addr)
 
 #ifndef ioread64_lo_hi
 #define ioread64_lo_hi ioread64_lo_hi
-static inline u64 ioread64_lo_hi(const void __iomem *addr)
+static inline u64 ioread64_lo_hi(const volatile void __iomem *addr)
 {
 	u32 low, high;
 
@@ -70,7 +70,7 @@ static inline u64 ioread64_lo_hi(const void __iomem *addr)
 
 #ifndef iowrite64_lo_hi
 #define iowrite64_lo_hi iowrite64_lo_hi
-static inline void iowrite64_lo_hi(u64 val, void __iomem *addr)
+static inline void iowrite64_lo_hi(u64 val, volatile void __iomem *addr)
 {
 	iowrite32(val, addr);
 	iowrite32(val >> 32, addr + sizeof(u32));
@@ -79,7 +79,7 @@ static inline void iowrite64_lo_hi(u64 val, void __iomem *addr)
 
 #ifndef ioread64be_lo_hi
 #define ioread64be_lo_hi ioread64be_lo_hi
-static inline u64 ioread64be_lo_hi(const void __iomem *addr)
+static inline u64 ioread64be_lo_hi(const volatile void __iomem *addr)
 {
 	u32 low, high;
 
@@ -92,7 +92,7 @@ static inline u64 ioread64be_lo_hi(const void __iomem *addr)
 
 #ifndef iowrite64be_lo_hi
 #define iowrite64be_lo_hi iowrite64be_lo_hi
-static inline void iowrite64be_lo_hi(u64 val, void __iomem *addr)
+static inline void iowrite64be_lo_hi(u64 val, volatile void __iomem *addr)
 {
 	iowrite32be(val, addr + sizeof(u32));
 	iowrite32be(val >> 32, addr);
