77bb51766a02fce9237269390b2b809203256e03
Krzysztof Kozlowski <krzk@kernel.org>
2020-09-21 21:13:43 +0200

    mfd: vx855: use PLATFORM_DEVID_NONE
    Use PLATFORM_DEVID_NONE define instead of "-1" value because:
     - it brings some meaning,
     - it might point attention why auto device ID was not used.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- drivers/mfd/vx855.c
+++ drivers/mfd/vx855.c
@@ -86,8 +86,8 @@ static int vx855_probe(struct pci_dev *pdev,
 	vx855_gpio_resources[1].start = gpio_io_offset + VX855_PMIO_R_GPO;
 	vx855_gpio_resources[1].end = vx855_gpio_resources[1].start + 3;
 
-	ret = mfd_add_devices(&pdev->dev, -1, vx855_cells, ARRAY_SIZE(vx855_cells),
-			NULL, 0, NULL);
+	ret = mfd_add_devices(&pdev->dev, PLATFORM_DEVID_NONE, vx855_cells,
+			      ARRAY_SIZE(vx855_cells), NULL, 0, NULL);
 
 	/* we always return -ENODEV here in order to enable other
 	 * drivers like old, not-yet-platform_device ported i2c-viapro */
