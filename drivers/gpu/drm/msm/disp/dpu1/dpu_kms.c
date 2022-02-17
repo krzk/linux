42f5f786d6afc5f1d9d965f34c0c4525b0882e4c
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2022-02-17 03:26:42 +0300

    drm/msm/dpu: add support for SM8450
    Add definitions for the display hardware used on Qualcomm SM8450
    platform.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
@@ -1313,6 +1313,7 @@ static const struct of_device_id dpu_dt_match[] = {
 	{ .compatible = "qcom,sc8180x-dpu", },
 	{ .compatible = "qcom,sm8150-dpu", },
 	{ .compatible = "qcom,sm8250-dpu", },
+	{ .compatible = "qcom,sm8450-dpu", },
 	{}
 };
 MODULE_DEVICE_TABLE(of, dpu_dt_match);
