25c716263fc5a4ac35390ab6b5a6df2c52ed1024
Bhupesh Sharma <bhupesh.sharma@linaro.org>
2022-06-01 12:55:16 +0530

    thermal: qcom: tsens: Add driver support for re-initialization quirk
    Since for some Qualcomm tsens controllers, its suggested to
    monitor the controller health periodically and in case an
    issue is detected, to re-initialize the tsens controller
    via trustzone, add the support for the same in the
    qcom tsens driver.
    
    Note that once the tsens controller is reset using scm call,
    all SROT and TM region registers will enter the reset mode.
    
    While all the SROT registers will be re-programmed and
    re-enabled in trustzone prior to the scm call exit, the TM
    region registers will not re-initialized in trustzone and thus
    need to be handled by the tsens driver.
    
    Cc: Bjorn Andersson <bjorn.andersson@linaro.org>
    Cc: Amit Kucheria <amitk@kernel.org>
    Cc: Thara Gopinath <thara.gopinath@gmail.com>
    Cc: linux-pm@vger.kernel.org
    Cc: linux-arm-msm@vger.kernel.org
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma@linaro.org>

--- drivers/thermal/qcom/tsens.h
+++ drivers/thermal/qcom/tsens.h
@@ -14,8 +14,11 @@
 #define SLOPE_FACTOR		1000
 #define SLOPE_DEFAULT		3200
 #define TIMEOUT_US		100
+#define RESET_TIMEOUT_MS	2
 #define THRESHOLD_MAX_ADC_CODE	0x3ff
 #define THRESHOLD_MIN_ADC_CODE	0x0
+
+#define CRITICAL_INT_EN		(BIT(2))
 
 #include <linux/interrupt.h>
 #include <linux/thermal.h>
@@ -165,6 +168,7 @@ enum regfield_ids {
 	/* ----- TM ------ */
 	/* TRDY */
 	TRDY,
+	FIRST_ROUND_COMPLETE,
 	/* INTERRUPT ENABLE */
 	INT_EN,	/* v2+ has separate enables for crit, upper and lower irq */
 	/* STATUS */
@@ -563,6 +567,14 @@ struct tsens_priv {
 	struct regmap			*srot_map;
 	u32				tm_offset;
 	bool				needs_reinit_wa;
+
+	struct work_struct		reinit_wa_notify;
+
+	/* protects reinit related serialization */
+	struct mutex			reinit_mutex;
+
+	/* lock for reinit workaround */
+	spinlock_t			reinit_lock;
 
 	/* lock for upper/lower threshold interrupts */
 	spinlock_t			ul_lock;
