krzk/linux

commit 317dd0dbadd8bd4de2d69fa6a0611456f3e15b1a

ASoC: samsung: speyside: Handle component name prefix
Use snd_soc_dapm_widget_name_cmp() helper when comparing widget names,
to include also the component's name prefix.

Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
Link: https://lore.kernel.org/r/20231023095428.166563-17-krzysztof.kozlowski@linaro.org
Signed-off-by: Mark Brown <broonie@kernel.org>

sound/soc/samsung/speyside.c
@@ -111,9 +111,9 @@ static int speyside_jack_polarity;
 static int speyside_get_micbias(struct snd_soc_dapm_widget *source,
 				struct snd_soc_dapm_widget *sink)
 {
-	if (speyside_jack_polarity && (strcmp(source->name, "MICB1") == 0))
+	if (speyside_jack_polarity && (snd_soc_dapm_widget_name_cmp(source, "MICB1") == 0))
 		return 1;
-	if (!speyside_jack_polarity && (strcmp(source->name, "MICB2") == 0))
+	if (!speyside_jack_polarity && (snd_soc_dapm_widget_name_cmp(source, "MICB2") == 0))
 		return 1;
 
 	return 0;

commit 7a6e36ec7867518d798d189d4daf97158b535343

NOT SURE: ASoC: samsung: Revert "ASoC: samsung: speyside: Handle component name prefix"
Revert commit 317dd0dbadd8bd4de2d69fa6a0611456f3e15b1a because it
incorrectly assumed that

Fixes: 317dd0dbadd8 ("ASoC: samsung: speyside: Handle component name prefix")
Cc: <stable@vger.kernel.org>
Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

sound/soc/samsung/speyside.c
@@ -111,9 +111,9 @@ static int speyside_jack_polarity;
 static int speyside_get_micbias(struct snd_soc_dapm_widget *source,
 				struct snd_soc_dapm_widget *sink)
 {
-	if (speyside_jack_polarity && (snd_soc_dapm_widget_name_cmp(source, "MICB1") == 0))
+	if (speyside_jack_polarity && (strcmp(source->name, "MICB1") == 0))
 		return 1;
-	if (!speyside_jack_polarity && (snd_soc_dapm_widget_name_cmp(source, "MICB2") == 0))
+	if (!speyside_jack_polarity && (strcmp(source->name, "MICB2") == 0))
 		return 1;
 
 	return 0;