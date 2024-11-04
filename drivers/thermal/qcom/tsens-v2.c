8ddeaed152c796e06a24e9a164b72884bdcc23b0
Manaf Meethalavalappu Pallikunhi <quic_manafm@quicinc.com>
2024-11-05 02:21:50 +0530

    thermal/drivers/tsens: update tsens threshold range for v2
    New platform can set threshold beyond 120C for tsens. Update tsens
    max and min threshold range up to controller allowed max and min
    threshold value.
    
    Signed-off-by: Manaf Meethalavalappu Pallikunhi <quic_manafm@quicinc.com>

--- drivers/thermal/qcom/tsens-v2.c
+++ drivers/thermal/qcom/tsens-v2.c
@@ -54,8 +54,8 @@ static struct tsens_features tsens_v2_feat = {
 	.adc		= 0,
 	.srot_split	= 1,
 	.max_sensors	= 16,
-	.trip_min_temp	= -40000,
-	.trip_max_temp	= 120000,
+	.trip_min_temp	= -204000,
+	.trip_max_temp	= 204000,
 };
 
 static struct tsens_features ipq8074_feat = {
