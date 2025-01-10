e766b38b7cef4573dc4f95093a3c19cd7c7ac6aa
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-01-10 16:13:16 +0100

    DEBUG: dispcc: try CLK_OPS_PARENT_ENABLE
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- drivers/gpu/drm/msm/dsi/dsi_cfg.c
+++ drivers/gpu/drm/msm/dsi/dsi_cfg.c
@@ -274,7 +274,8 @@ static const struct msm_dsi_host_cfg_ops msm_dsi_6g_v2_host_ops = {
 };
 
 static const struct msm_dsi_host_cfg_ops msm_dsi_6g_v2_9_host_ops = {
-	.link_clk_set_rate = dsi_link_clk_set_rate_6g_v2_9,
+	.link_clk_set_rate = dsi_link_clk_set_rate_6g,
+	//.link_clk_set_rate = dsi_link_clk_set_rate_6g_v2_9,
 	.link_clk_enable = dsi_link_clk_enable_6g,
 	.link_clk_disable = dsi_link_clk_disable_6g,
 	.clk_init_ver = dsi_clk_init_6g_v2_9,
