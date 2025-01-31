542455ace7519ba745609f54ee4c29f5072a0129
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-01-31 14:22:05 +0100

    drm/msm/dsi/phy: Do not overwite PHY_CMN_CLK_CFG1 when choosing bitclk source
    PHY_CMN_CLK_CFG1 register has four fields being used in the driver: DSI
    clock divider, source of bitclk and two for enabling the DSI PHY PLL
    clocks.
    
    dsi_7nm_set_usecase() sets only the source of bitclk, so should leave
    all other bits untouched.  Use newly introduced
    dsi_pll_cmn_clk_cfg1_update() to update respective bits without
    overwriting the rest.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- drivers/gpu/drm/msm/dsi/phy/dsi_phy_7nm.c
+++ drivers/gpu/drm/msm/dsi/phy/dsi_phy_7nm.c
@@ -615,7 +615,6 @@ static int dsi_7nm_pll_restore_state(struct msm_dsi_phy *phy)
 static int dsi_7nm_set_usecase(struct msm_dsi_phy *phy)
 {
 	struct dsi_pll_7nm *pll_7nm = to_pll_7nm(phy->vco_hw);
-	void __iomem *base = phy->base;
 	u32 data = 0x0;	/* internal PLL */
 
 	DBG("DSI PLL%d", pll_7nm->phy->id);
@@ -634,7 +633,7 @@ static int dsi_7nm_set_usecase(struct msm_dsi_phy *phy)
 	}
 
 	/* set PLL src */
-	writel(data << 2, base + REG_DSI_7nm_PHY_CMN_CLK_CFG1);
+	dsi_pll_cmn_clk_cfg1_update(pll_7nm, GENMASK(3, 2), data << 2);
 
 	return 0;
 }
