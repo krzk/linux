bdac457f5f1a70f6eaed6b63dd6b61cc4b18ef94
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-02-21 13:31:03 +0100

    drm/msm/dpu: Clear CTL_FETCH_PIPE_ACTIVE on mixer reset
    Resetting mixers should also include resetting active fetch pipes.
    
    Fixes: ae4d721ce100 ("drm/msm/dpu: add an API to reset the encoder related hw blocks")
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    
    ---
    
    Changes in v3:
    1. New patch, split from previous big DPU v12.0.

--- drivers/gpu/drm/msm/disp/dpu1/dpu_encoder.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_encoder.c
@@ -2199,6 +2199,9 @@ static void dpu_encoder_helper_reset_mixers(struct dpu_encoder_phys *phys_enc)
 		/* clear all blendstages */
 		if (phys_enc->hw_ctl->ops.setup_blendstage)
 			phys_enc->hw_ctl->ops.setup_blendstage(ctl, hw_mixer[i]->idx, NULL);
+
+		if (ctl->ops.set_active_fetch_pipes)
+			ctl->ops.set_active_fetch_pipes(ctl, NULL);
 	}
 }
 
