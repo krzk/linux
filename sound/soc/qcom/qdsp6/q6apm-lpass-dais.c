27a4fc4cb2b8607443ac9d206e4260aef4e36cca
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-02-27 09:02:28 +0100

    Revert "ASoC: qdsp6: lpass-dai: check if params have changed before re-prepare"
    This reverts commit 3affc80370df1eea565afd1b91ebd36cd4b0f9fe.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- sound/soc/qcom/qdsp6/q6apm-lpass-dais.c
+++ sound/soc/qcom/qdsp6/q6apm-lpass-dais.c
@@ -21,7 +21,6 @@
 struct q6apm_lpass_dai_data {
 	struct q6apm_graph *graph[APM_PORT_MAX];
 	bool is_port_started[APM_PORT_MAX];
-	bool params_changed[APM_PORT_MAX];
 	struct audioreach_module_config module_config[APM_PORT_MAX];
 };
 
@@ -138,7 +137,6 @@ static int q6dma_hw_params(struct snd_pcm_substream *substream,
 	cfg->sample_rate = params_rate(params);
 	cfg->num_channels = channels;
 	audioreach_set_default_channel_mapping(cfg->channel_map, channels);
-	dai_data->params_changed[dai->id] = true;
 
 	return 0;
 }
@@ -170,10 +168,6 @@ static int q6apm_lpass_dai_prepare(struct snd_pcm_substream *substream, struct s
 	int rc;
 
 	if (dai_data->is_port_started[dai->id]) {
-		/* Check we really need to do costly stop and start of graph */
-		if (!dai_data->params_changed[dai->id])
-			return 0;
-
 		q6apm_graph_stop(dai_data->graph[dai->id]);
 		dai_data->is_port_started[dai->id] = false;
 
@@ -182,8 +176,6 @@ static int q6apm_lpass_dai_prepare(struct snd_pcm_substream *substream, struct s
 			dai_data->graph[dai->id] = NULL;
 		}
 	}
-
-	dai_data->params_changed[dai->id] = false;
 
 	/**
 	 * It is recommend to load DSP with source graph first and then sink
