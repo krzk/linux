40cb43c05bea847a02390449f4ad66494746208c
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-03-05 22:38:38 +0100

    clk: samsung: exynos990: Use platform_driver_probe() to avoid __refdata
    Switch registering platform driver to platform_driver_probe(), so the
    'struct platform_driver' can be properly discarded after init and there
    won't be need of __refdata to silence DEBUG_SECTION_MISMATCH.
    
    The change requires using subsys_initcall instead of core_initcall,
    because no device drivers would bound in the latter, as required by
    platform_driver_probe().
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- drivers/clk/samsung/clk-exynos990.c
+++ drivers/clk/samsung/clk-exynos990.c
@@ -1504,18 +1504,17 @@ static const struct of_device_id exynos990_cmu_of_match[] = {
 	{ },
 };
 
-static struct platform_driver exynos990_cmu_driver __refdata = {
+static struct platform_driver exynos990_cmu_driver __initdata = {
 	.driver	= {
 		.name = "exynos990-cmu",
 		.of_match_table = exynos990_cmu_of_match,
 		.suppress_bind_attrs = true,
 	},
-	.probe = exynos990_cmu_probe,
 };
 
 static int __init exynos990_cmu_init(void)
 {
-	return platform_driver_register(&exynos990_cmu_driver);
+	return platform_driver_probe(&exynos990_cmu_driver, exynos990_cmu_probe);
 }
 
-core_initcall(exynos990_cmu_init);
+subsys_initcall(exynos990_cmu_init);
