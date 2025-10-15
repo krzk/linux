d998beca5ad9448a2fc814e5e7b4d1381ea61d59
David Sterba <dsterba@suse.com>
2025-10-15 18:48:37 +0200

    btrfs: move and rename CSUM_FMT definition
    Move the CSUM_FMT* definitions to fs.h where will be the BTRFS_KEY_FMT
    and add the prefix for consistency.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/btrfs_inode.h
+++ fs/btrfs/btrfs_inode.h
@@ -543,10 +543,6 @@ static inline void btrfs_set_inode_mapping_order(struct btrfs_inode *inode)
 #endif
 }
 
-/* Array of bytes with variable length, hexadecimal format 0x1234 */
-#define CSUM_FMT				"0x%*phN"
-#define CSUM_FMT_VALUE(size, bytes)		size, bytes
-
 void btrfs_calculate_block_csum_folio(struct btrfs_fs_info *fs_info,
 				      const phys_addr_t paddr, u8 *dest);
 void btrfs_calculate_block_csum_pages(struct btrfs_fs_info *fs_info,
